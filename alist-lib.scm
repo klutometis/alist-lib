0936103dc8d583602b398454b9dc846ee1ac9b72
Peter Danenberg <pcd@roxygen.org>
2012-04-29 22:24:49 -0700

    Actually specify the exports.

--- alist-lib.scm
+++ alist-lib.scm
@@ -1,6 +1,16 @@
 (module
  alist-lib
- *
+ (alist-values
+  alist-keys
+  alist-map
+  alist-set!
+  alist-update!
+  alist-update!/default
+  alist-ref
+  alist-ref/default
+  alist-size
+  alist-fold
+  alist-set)
  (import scheme
          chicken)
  (use srfi-1 debug matchable)
